fbb42df1dccf22d7034f6da3b9a7f376ccce40d5
lcko1012 <lecanhkieuoanh@gmail.com>
2021-02-28 13:17:57 +0700

    upload avatar

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(fileUpload({
 
 //Routes
 app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/upload'))
+
 
 app.use('/', (req, res, next) => {
     res.json({
