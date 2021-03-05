7bebc1edb50981af10a713bbb7adce873c578242
lcko1012 <lecanhkieuoanh@gmail.com>
2021-03-06 00:45:58 +0700

    Header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <header>
+            <div className="logo">
+                <h1><Link to="/">KO SHOP</Link></h1>
+            </div>
+
+            <ul>
+                <li>
+                    <Link to="/"><i class="fas fa-shopping-cart"></i>Cart</Link>
+                </li>
+                <li>
+                    <Link to="/"><i class="fas fa-user"></i>Sign in</Link>
+                </li>
+            </ul>
+        </header>
     )
 }
 
